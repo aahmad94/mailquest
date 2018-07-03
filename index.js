0b68847063827dd6b71d8924a4f834a422d028d4
aahmad94 <aka5vm@virginia.edu>
2018-07-02 19:53:34 -0700

    tweak google strategy settings for heroku proxy

--- index.js
+++ index.js
@@ -24,3 +24,8 @@ require('./routes/auth_routes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+if (process.env.NODE_ENV === 'production') {
+  console.log("hello");
+} else {
+  console.log("world");
+}
